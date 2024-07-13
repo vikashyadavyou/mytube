b4741889f02b4747f31a7e6c28bd01e7658e051c
Yadav Vikash <yadavvikashyou@gmail.com>
2024-07-13 16:35:28 +0530

    upload from data add in object

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './css/navbar.css'
-import upload from '../pages/upload';
-
+import Upload from '../pages/upload';
 const navbar=()=>{
     return(
         <div className='navbar'>
             <h1>My Tube</h1>
-            <h2 onClick={upload}>Upload Video</h2>
-        
+            
             
         </div>
     )
--- src/components/pages/upload.jsx
+++ src/components/pages/upload.jsx
@@ -1,20 +1,49 @@
+import { useState } from 'react';
 import React from 'react'
 import './upload.css';
 
-const upload = () => {
+const Upload = () => {
+  
+
+    const [formData, setFormData] = useState({
+      Title: '',
+      Description: '',
+      Video: "",
+    })
+    const HandleInputChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]:value
+      });
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setFormData({
+      Title: '',
+      Description: '',
+      Video: '',
+      
+      })
+      console.log(formData);
+  } 
   return (
     <div className='form'>
-      <form>
+      <form onSubmit={handleSubmit}>
       <h1 className='Upload_form'>Upload Form</h1>
         <label htmlFor="title">Title</label>
-        <input id='title' type='text' placeholder='Title'/><br/><br/>
+        <input type='text' id="title" name="Title" onChange={HandleInputChange}  value={formData.Title} placeholder="Title"/><br/><br/>
         <label htmlFor="description">Description</label>
-        <input type='text' id='description' placeholder='Description'/><br/><br/>
-        <input type="file" name="" id="video_file" />
-        <button type="submit">Upload</button>
+        <input type='text' id="description" name="Description" value={formData.Description} onChange={HandleInputChange} placeholder= "Description" /> <br/><br/>
+        <input type="file" id="video_file" name="Video" value={formData.Video} onChange={HandleInputChange}/>
+        <button type="submit" >Upload</button>
       </form>
     </div>
   )
-}
 
-export default upload
+  }
+    
+
+
+export default Upload
